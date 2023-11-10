faaf62db0fa4b4e8bf9e3607fb432933c1d737b6
levannghia <levannghia.tdc2018@gmail.com>
2023-11-10 15:21:19 +0700

    Fix ESLink

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,13 @@ module.exports = {
   root: true,
   extends: '@react-native',
   rules: {},
-};
+  env: {
+    development: {
+      // Tắt ESLint khi ở chế độ phát triển
+      eslintrc: false,
+    },
+    production: {
+      // Cấu hình ESLint khi ở chế độ sản xuất
+    },
+  }
+}
